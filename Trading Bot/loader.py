90b130e1c10644754b1d12c662150435ef59305c
14iq <60758749+14iq@users.noreply.github.com>
2021-08-23 03:03:50 +0300

    suckdick

--- Trading Bot/loader.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import ParseMode
-from data.config import config
-
-
-bot = Bot(config("api_token"), parse_mode=ParseMode.HTML)
-dp = Dispatcher(bot, storage=MemoryStorage())
