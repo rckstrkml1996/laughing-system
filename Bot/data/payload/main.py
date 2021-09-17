786edb08f9cd087051f79815f9a432327e307cd3
14iq <60758749+14iq@users.noreply.github.com>
2021-09-17 19:28:44 +0300

    mafaka

--- Bot/data/payload/main.py
+++ Bot/data/payload/main.py
@@ -6,7 +6,7 @@ from config import config
 
 
 new_username_text = emojize(
-    ":sneezing_face: <a href='tg://user?id={cid}'>{name}</a> сменил свой ник с <b>{old_username}</b> на <b>{new_username}</b>"
+    ":sneezing_face: <a href='tg://user?id={chat_id}'>{name}</a> сменил свой ник с <b>{old_username}</b> на <b>{new_username}</b>"
 )
 
 # multy use
