5dbc8adee953ccd9cb2940197853c41a230b3e0e
14iq <60758749+14iq@users.noreply.github.com>
2021-10-23 15:41:41 +0300

    fix auth by cancelling state

--- EscortBot/handlers/welcome.py
+++ EscortBot/handlers/welcome.py
@@ -1,8 +1,8 @@
 from aiogram import types
 from aiogram.utils.markdown import quote_html
+from aiogram.dispatcher import FSMContext
 
 from loader import dp
-
 from config import config
 from data.states import Login
 from data.payload import new_user_text, new_user_wrong_code_text, welcome_text
@@ -17,7 +17,7 @@ async def welcome_new_user(message: types.Message):
 
 
 @dp.message_handler(state=Login.code, is_user=False)
-async def welcome_code(message: types.Message):
+async def welcome_code(message: types.Message, state: FSMContext):
     worker = get_worker_by_code(message.text)
 
     if worker:
@@ -28,13 +28,17 @@ async def welcome_code(message: types.Message):
             quote_html(message.chat.full_name),
         )
         if created:
-            await welcome_msg(message)
+            await welcome_msg(message, state)
+        else:
+            await message.answer(new_user_wrong_code_text)
     else:
         await message.answer(new_user_wrong_code_text)
 
 
-@dp.message_handler(commands=["start", "help"])  # its - is_user=True
-async def welcome_msg(message: types.Message):
+@dp.message_handler(commands=["start", "help"], state="*")  # its - is_user=True
+async def welcome_msg(message: types.Message, state: FSMContext):
+    await state.finish()
+
     fullname = quote_html(message.chat.full_name)  # it may contain < and > and ..
 
     await message.answer(
