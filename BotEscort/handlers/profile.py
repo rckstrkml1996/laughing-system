3ac83058782594a6e6a624257d86d0472b6baf8a
14iq <60758749+14iq@users.noreply.github.com>
2021-11-09 10:13:19 +0300

    profile

--- BotEscort/handlers/profile.py
+++ BotEscort/handlers/profile.py
@@ -1,7 +1,7 @@
 from aiogram.types import CallbackQuery
 from aiogram.utils.exceptions import MessageNotModified
 
-from loader import dp
+from loader import dp, config
 from data.texts import profile_text, already_in_text
 from data.keyboards import welcome_keyboard
 
@@ -13,7 +13,9 @@ async def profile(query: CallbackQuery):
             profile_text.format(
                 chat_id=query.from_user.id, username=query.from_user.username
             ),
-            reply_markup=welcome_keyboard,
+            reply_markup=welcome_keyboard(
+                config.escort_sup_username, config.escotz_link
+            ),
         )
     except MessageNotModified:
         await query.answer(already_in_text)
